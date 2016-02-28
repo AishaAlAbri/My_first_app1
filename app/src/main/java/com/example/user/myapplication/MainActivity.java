920023d9bcbec625f1d39324e76eba1a3c10f9cd
Aisha Al-Abri <Aisha Al-Abri>
2016-02-28 17:16:06 +0400

    Initial commit

---
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.user.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button submitBtn =(Button)(findViewById(R.id.submit));
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
