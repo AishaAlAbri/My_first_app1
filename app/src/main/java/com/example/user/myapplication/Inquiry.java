827b562b32fda24341e037588bfc8b5241425947
badaaif <badaaif@gmail.com>
2016-03-01 12:53:25 +0400

    send function for inquiry activity

--- app/src/main/java/com/example/user/myapplication/Inquiry.java
+++ app/src/main/java/com/example/user/myapplication/Inquiry.java
@@ -1,22 +1,34 @@
 package com.example.user.myapplication;
 
 import android.app.DatePickerDialog;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import java.io.ByteArrayOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import cz.msebera.android.httpclient.Header;
+
 
 public class Inquiry extends AppCompatActivity {
 
-    private EditText etDate;
+    private EditText etName,etPhone,etEduInstitute,etSubject,etReplyDate;
+    private Spinner spSupervisor;
     private SimpleDateFormat dateFormatter;
     private DatePickerDialog datePickerDialogStart;
     private DrawingView drawingView;
@@ -31,7 +43,13 @@ public class Inquiry extends AppCompatActivity {
         linearDrawing = (LinearLayout) findViewById(R.id.linearDrawing);
         linearDrawing.addView(drawingView);
 
-        etDate = (EditText) findViewById(R.id.editTextDate);
+        spSupervisor = (Spinner) findViewById(R.id.spSupervisor);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etPhone = (EditText) findViewById(R.id.etPhone);
+        etEduInstitute = (EditText) findViewById(R.id.etEduInstitute);
+        etSubject = (EditText) findViewById(R.id.etSubject);
+        etReplyDate = (EditText) findViewById(R.id.etReplyDate);
         dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
         setDateField();
 
@@ -41,7 +59,7 @@ public class Inquiry extends AppCompatActivity {
     private void setDateField() {
         final Calendar newCalendar = Calendar.getInstance();
 
-        etDate.setOnClickListener(new View.OnClickListener()
+        etReplyDate.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
@@ -61,7 +79,7 @@ public class Inquiry extends AppCompatActivity {
                 try {
                     Calendar newDate = Calendar.getInstance();
                     newDate.set(year, monthOfYear, dayOfMonth);
-                    etDate.setText(dateFormatter.format(newDate.getTime()));
+                    etReplyDate.setText(dateFormatter.format(newDate.getTime()));
                 }catch (Exception e)
                 {
                     e.printStackTrace();
@@ -70,5 +88,58 @@ public class Inquiry extends AppCompatActivity {
 
         },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
 
+    }
+
+    public void onSend(View v)
+    {
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("Insert", getSqlStatement());
+        client.post("http://104.197.212.107:3000/Insert", params, new TextHttpResponseHandler()
+                {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, String res)
+                    {
+                        // called when response HTTP status is "200 OK
+                        //tvResult.setText(res);
+                        Log.d("Result: ",res);
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String res, Throwable t)
+                    {
+                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                        Log.d("Error: ", res);
+                    }
+                }
+        );
+    }
+
+    private String getSqlStatement()
+    {
+        String name = etName.getText().toString();
+        String phone = etPhone.getText().toString();
+        Date currentDate = new Date();
+        String date = dateFormatter.format(currentDate);
+        String eduInstitute = etEduInstitute.getText().toString();
+        String subject = etSubject.getText().toString();
+        String supervisor = spSupervisor.getSelectedItem().toString();
+        String replyDate = etReplyDate.getText().toString();
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        drawingView.getDrawingCache().compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream .toByteArray();
+        String signature = Base64.encodeToString(byteArray, Base64.DEFAULT);
+
+
+        String sql ="INSERT INTO users (name,phone,date,institute,subject,supervisor," +
+                "replayDate,signature) values " +
+                "('"+name+"',"+phone+",'"+date+"','"+eduInstitute+"'," +
+                "'"+subject+"','"+supervisor+"','"+replyDate+"','"+signature+"')";
+
+
+
+        return sql;
     }
 }
--- app/src/main/java/com/example/user/myapplication/SubmittedDetails.java
+++ app/src/main/java/com/example/user/myapplication/SubmittedDetails.java
@@ -2,16 +2,39 @@ package com.example.user.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SubmittedDetails extends AppCompatActivity
 {
 
     private InquiryData inquiryData;
+    private TextView tvName,tvPhone,tvDate,tvInstitute,tvSubject,tvSupervisor,tvReplayDate;
+    private ImageView imageSignature;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submitted_details);
         inquiryData = (InquiryData)getIntent().getSerializableExtra("data");
+
+        tvName = (TextView) findViewById(R.id.tvName);
+        tvPhone = (TextView) findViewById(R.id.tvPhone);
+        tvDate = (TextView) findViewById(R.id.tvDate);
+        tvInstitute = (TextView) findViewById(R.id.tvInstitute);
+        tvSubject = (TextView) findViewById(R.id.tvSubject);
+        tvSupervisor = (TextView) findViewById(R.id.tvSupervisor);
+        tvReplayDate = (TextView) findViewById(R.id.tvReplyDate);
+        imageSignature = (ImageView) findViewById(R.id.imageSignature);
+
+
+        tvName.setText("Name: " +inquiryData.getName());
+        tvPhone.setText("Phone: " +inquiryData.getPhone());
+        tvDate.setText("Date: " +inquiryData.getDate());
+        tvInstitute.setText("Institute: " +inquiryData.getInstitute());
+        tvSubject.setText("Subject: " +inquiryData.getSubject());
+        tvSupervisor.setText("Supervisor: " +inquiryData.getSupervisor());
+        tvReplayDate.setText("Replay Date: " +inquiryData.getReplyDate());
+
     }
 }
